14d8ba3ace0986c525c14e5b870f5d24a427d031
Shewer Lu <shewer@gmail.com>
2020-08-22 00:37:59 +0800

    move schema get set func to  schema_func & 修改 init()
    Signed-off-by: Shewer Lu <shewer@gmail.com>

--- lua/muti_reverse/load_schema.lua
+++ lua/muti_reverse/load_schema.lua
@@ -5,41 +5,16 @@
 --
 -- Distributed under terms of the MIT license.
 --
-local function getdata(config,key,datatype)
-	datatype = datatype or "string"
-	if "int" == datatype or "number" == datatype then 
-		return config:get_int(key)
-	elseif  "dobule" == datatype then 
-		return config:get_double(key)
-	elseif "bool"  == datatype or  "boolean" == datatype then 
-		return config:get_bool(key)
-	elseif "string" == datatype then 
-		return config:get_string(key)
-	else 
-	    return nil 
-	end 
-end 
 
-local function get_list(config,key,datatype)
-	datatype = datatype or "string"
-	if not config:is_list(key) then return nil end 
-	local tlocal ab=metatable()
-	local list_size=config:get_list_size( key  )
-	local tab=metatable()
-	for i=0,(list_size -1) , 1 do
-		tab:insert(  getdata( config, key .. "/@" .. i, datatype ) )
-	end 
-	return tab
-
-end
-
-
+local schema_func=require('muti_reverse.schema_func')
 
 
 
 
 
 local function init_data(env)
+	local get_list=schema_func.get_list
+	local get_data=schema_func.get_data
 
 	local tab=metatable()
 	local config=env.engine.schema.config
@@ -55,19 +30,26 @@ local function init_data(env)
 
 	tab:insert(1,"translator") -- main dictionarypy  insert to first
     --   init-- 
-	local key1= "dictionary"
-	local key2= "preedit_format"
-	local key3= "textkey"
-	local key4= "hotkey"
+	local keys=metatable( { 
+		dbname= {"dictionary", "string"},
+		pattern= {"preedit_format","list","string"},
+		pattern2={"comment_format", "list","string"},
+		text= {"textkey","string"},
+		hotkey= {"hotkey","string"},
+		tips={ "tips","string"},
+	})
+
+
 	local tran_tab=tab:map(function(elm)  -- elm : segment 
-		return  {
-			seg= elm ,
-			dbname = getdata( config , elm .."/".. key1 ), -- default "string"	
-			pattern = get_list(config, elm .."/".. key2 ), -- default "string"
-			text= getdata(config,elm .."/".. key3),
-			hotkey=getdata(config,elm.."/".. key4),
-		}
-	end) 
+		    local  ttab=  metatable()
+			ttab["seg"]=elm
+			keys:each(function(v,k)  --  insert ttab[k] = getdata() 
+				ttab[k]=  get_data(config,  elm .. "/" .. v[1] ,v[2], v[3] )   --config,key,  elm segment .. / subseg ,
+			end )
+			return ttab
+
+		end )
+
 	return tran_tab
 end 
 
