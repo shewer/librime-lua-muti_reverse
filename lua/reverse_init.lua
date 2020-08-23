85e56adb020872458130c18a50287b45f4e56574
Shewer Lu <shewer@gmail.com>
2020-08-23 16:53:28 +0800

    將 Switch:inser  非 filterlist obj 移至 init_proc 註冊 , schema:each(func... --> schema:each( init_reversefilter)
    Signed-off-by: Shewer Lu <shewer@gmail.com>

--- lua/reverse_init.lua
+++ lua/reverse_init.lua
@@ -8,11 +8,12 @@
 
 
 -- bopomofo  filterlist     { reversedb_filter  , qcodefilter, reverse_filter }
-
+local ON=true
+local OFF=false
 require 'muti_reverse'
 --
 -- 
-	
+
 
 --switch:insert({hotkey="Control+0", text=nil,obj=qcode,method="toggle",argv=nil})
 --switch:insert({hotkey="Control+9", text=nil,obj=filter_switch,method="next",argv=nil})
@@ -35,70 +36,84 @@ end
 local function disenable_completion(cand) 
 	return "",str
 end 
+
+
 --[[
 --  candinfo filter 
 local candinfo=FFilter:new( candinfo_func,false)  
- --comment_off   filter 
+--comment_off   filter 
 local comment_off =FFilter:new(disenable_completion,false)
 --ENV= ENV or false
- --簡碼 filter
+--簡碼 filter
 local qcode=FFilter:new(require('muti_reverse.qcode') ,false)
 local switch=Switch:new(),   
 local filter_switch=FilterList_switch:new() 
 --]]
+function candinfo(self)
 
 
+end 
 local _tab={
 	switch=Switch:new(),   
 	filter_switch=FilterList_switch:new()  ,
-	candinfo=FFilter:new(candinfo,false)  ,
-	comment_off =FFilter:new(disenable_completion,false),
-    qcode=FFilter:new(require('muti_reverse.qcode') ,false),
+	qcode= FFilter:new(require('muti_reverse.qcode') ,ON),
 	dbfile=DBFilter,
-	
+	--candinfo=FFilter:new(candinfo,false)  ,
+	--comment_off =FFilter:new(disenable_completion,false),
+	--qcode=FFilter:new(require('muti_reverse.qcode') ,false),
+
 }
+local function init_reverfilter(elm,i )
+	-- 設定 及註冊 主副反查字典   
+	--schema:each( function( elm,i) 
+		local revf=_tab 
+		local switch=revf.switch
+		local filter_switch=revf.filter_switch
+
+		local hotkey= elm.hotkey=="" and nil or elm.hotkey -- 空字串  設 nil
+		local text = elm.text== "" and  "V".. tostring(i) or elm.text -- 空字串 設  V1 ..... Vn 
+		switch:remove_key(hotkey)
+		switch:remove_text(text)
+
+		local dbf= DBFilter:new({dbname=elm.dbname} ,ON) 
+		local qcode= revf.qcode
+		local psf=PSFilter:new(elm.pattern,ON) 
+		local flist = FilterList:new({dbf,qcode,psf} ,ON) -- ReverseDb  qcode 轉碼 串在一起
+		flist:name(elm.dbname) 
+		-- insert  & reg 
+		filter_switch:insert(flist) -- 加入 反查字典 切換開關
+		switch:insert({hotkey= hotkey ,text=text  ,obj=filter_switch,method="set_filter",argv=flist}) -- 註冊 熱鍵 快鍵
+end 
+
 --  reverse filter  init 
-function _tab:open(schema )   --  env 空值時 可以自設
-	schema = schema or require('muti_reverse.schema_sim')    -- 自設  檔也可改檔名  資料格式 依此檔
-	metatable(schema)
+function _tab:open_revdb(schema )   --  env 空值時 可以自設
+	--self.dbfile= Dbfilter
+	if not schema then 
+		log.info( string.format("schema is nil  defalult load schema form : schema_sim" )) 
+		schema = require('muti_reverse.schema_sim')    -- 自設  檔也可改檔名  資料格式 依此檔
+	end 
 	-- 註冊 hotkey & text 
-	self.schema=schema 
-    -- 增加 獨立 主字典反查 用於 簡碼提示功能
+	self.schema=metatable(schema )
+	-- 增加 獨立 主字典反查 用於 簡碼提示功能
 	self.mainfilter= FilterList:new {
-		DBFilter:new(schema[1] ,true ), 
-		FFilter:new(require('muti_reverse.qcode') ,true),
-		psf=PSFilter:new(schema[1].pattern,true),
+		DBFilter:new(schema[1] ,ON), 
+		FFilter:new(require('muti_reverse.qcode') ,ON),
+		psf=PSFilter:new(schema[1].pattern,ON),
 	}
 
-	local switch= self.switch
-	local qcode=self.qcode 
-	local filter_switch=self.filter_switch
-	switch:insert({hotkey="Control+0", text="Vq",obj=qcode,method="toggle",argv=nil})    -- 短碼開關
-	switch:insert({hotkey="Control+9", text=nil,obj=filter_switch,method="next",argv=nil}) -- 下一反查
-	switch:insert({hotkey="Control+8", text=nil,obj=filter_switch,method="prev",argv=nil}) -- 上一反查
-	switch:insert({hotkey="Control+7", text="VV",obj=filter_switch,method="toggle",argv=nil}) -- 反查開關
-	switch:insert({hotkey="Control+6", text="Va",obj=candinfo,method="toggle",argv=nil}) -- comment開關
-	switch:insert({hotkey="Control+5", text="Vd",obj=comment_off,method="toggle",argv=nil}) -- canddata 開關
+	self.dbfile.reset()
+	self.filter_switch:reset()
 
 	-- 設定 及註冊 主副反查字典   
 	--schema:each( function( elm,i) 
-	self.schema:each( function( elm,i) 
-
-		local hotkey= elm.hotkey=="" and nil  -- 空字串  設 nil
-		local text = elm.text== "" and  "V".. tostring(i)  -- 空字串 設  V1 ..... Vn 
-		local dbf= DBFilter:new({dbname=elm.dbname} ,true ) -- {dbname="字典檔名" } ex"{dbname="cangjie5" } ==> cangjie5.reverse.bin
-		local psf=PSFilter:new(elm.pattern,true) -- { "pattern1", "pattern2" .....} 
-		
-		local flist = FilterList:new({dbf,qcode,psf} ,true ) -- ReverseDb  qcode 轉碼 串在一起
-	    -- insert  & reg 
-		filter_switch:insert(flist) -- 加入 反查字典 切換開關
-		switch:insert({hotkey= hotkey ,text=txet  ,obj=filter_switch,method="set_filter",argv=filst}) -- 註冊 熱鍵 快鍵
-	end )
+	self.schema:each(init_reverfilter    ) -- func(elm,index) 
+	
+	self.dbfile.Open() -- open RevseDb
 
 
-	 self.dbfile.Open() -- open RevseDb
-	 --self.DBFilter.Open()
- end 
+	FILTER= FILTER or self.filter_switch
+	--self.DBFilter.Open()
+end 
 
 
 
